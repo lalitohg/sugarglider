cc958fee2af2639ca2d1ade705a6bf6368563f47
Eduardo Hernández Guerrero <eduardohdzgro@gmail.com>
2020-01-29 13:17:21 -0600

    Set api and db up on docker compose file

--- api/conf/default-env-vars.js
+++ api/conf/default-env-vars.js
@@ -1,8 +1,10 @@
 module.exports = {
     SUGARGLIDER_API_PORT: 3000,
-    NEO4J_HOST: "localhost",
-    NEO4J_BOLT_PORT: "",
-    NEO4J_DB_NAME: "",
-    NEO4J_DB_USER: "",
-    NEO4J_DB_PASS: "",
+    NEO4J_HOST: 'localhost',
+    NEO4J_HTTP_API_PORT: 7474,
+    NEO4J_BOLT_PORT: 7687,
+    NEO4J_DB_NAME: '',
+    NEO4J_AUTH_TYPE: 'none',
+    NEO4J_DB_USER: '',
+    NEO4J_DB_PASS: '',
 }
--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ const envConf = require('./conf/env-conf')
 const expressConf = require('./conf/express-conf')
 
 const bootReport = () => {
-    console.log('\nAll set!\n')
-    console.log(`Server is now listening on http://localhost:${process.env.SUGARGLIDER_API_PORT}\n`)
-    process.env.NODE_ENV === 'dev' && console.log(`GraphiQL is on an availabe at http://localhost:${process.env.SUGARGLIDER_API_PORT}/graphql\n`)
-
+    return new Promise((resolve, reject) => {
+        console.log('\nAll set!\n')
+        console.log(`Server is now listening on http://0.0.0.0:${process.env.SUGARGLIDER_API_PORT}\n`)
+        process.env.NODE_ENV === 'dev' && console.log(`GraphiQL is on an availabe at http://0.0.0.0:${process.env.SUGARGLIDER_API_PORT}/graphql\n`)
+        return resolve(true)
+    })
+    
 }
 
 const main = () => {
